8eec9238e4878d7b7986ef731a1c32940e29ce93
Sathish Myilsamy <sathishmyilsamy21@gmail.com>
2024-03-28 19:56:04 +0530

    TEXT ANIMATION

---
+++ 01-JAVASCRIPT_PROJECTS/09-TEXT_ANIMATION_PROJECT/dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var heading = document.querySelector('.heading');
+var text = heading.textContent;
+var alpha = text.split("");
+heading.textContent = "";
+
+for (i = 0; i < alpha.length; i++) {}
